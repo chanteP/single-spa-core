bc5410d4eb86b94fa1b1be906bf752e2c00ac0b4
chenwenbin03 <chenwenbin03@meituan.com>
2019-01-30 19:54:47 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+import "antd/dist/antd.css";
+
+import { Switch } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Switch defaultChecked onChange={onChange} />
+        12312
       </div>
     );
   }
 }
 
 export default App;
+
+
+function onChange(checked) {
+  console.log(`switch to ${checked}`);
+}
